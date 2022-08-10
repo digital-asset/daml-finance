a2abbaa90b6ff0829d7625f364def0a17c47da66
Matteo Limberto <m.limberto@gmail.com>
2022-08-10 13:55:16 +0200

    fixup

--- scripts/generate-doc.sh
+++ scripts/generate-doc.sh
@@ -1,20 +1,32 @@
+#!/bin/bash
 # Copyright (c) 2022 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-# Test
+# This script should be run from the repo root directory.
+# Example usage: ./scripts/generate-doc.sh ../daml/bazel-bin/compiler/damlc/daml-base-anchors.json
 
+set -eu
+
+# The anchor file is the `daml-base-anchors.json` file shipped with the SDK.
+# It can also be found in ../daml/bazel-bin/compiler/damlc/daml-base-anchors.json after successfully building the daml repo
+anchor_file=$1
+
+echo "Generating documentation in json format."
+make docjson
+
+echo "Convert json documentation to rst."
 daml damlc docs \
-  --output=daml-finance-rst \
+  --output=.docs/daml-finance-rst \
   --input-format=json \
   --format=Rst \
   --exclude-instances=HasField \
   --drop-orphan-instances \
-  --template=base-rst-template.rst \
-  --index-template=base-rst-index-template.rst \
+  --template=.docs/base-rst-template.rst \
+  --index-template=.docs/base-rst-index-template.rst \
   --base-url=https://docs.daml.com/daml/daml-finance \
-  --input-anchor=../daml/bazel-bin/compiler/damlc/daml-base-anchors.json \
+  --input-anchor=${anchor_file} \
   .docs/daml-finance.json
-  # --hoogle-template=$./ \
-  # --output-hoogle=./ \
-  # --output-anchor=./ \
+  # --hoogle-template= \
+  # --output-hoogle= \
+  # --output-anchor= \
   # --target=1.dev \
