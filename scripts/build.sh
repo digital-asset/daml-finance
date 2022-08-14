74be90e1e3dff7609d40f19c9e9a49cfcfba5093
Georg Schneider <georg.schneider@gmail.com>
2022-08-14 08:48:26 +0200

    Add instrument factories

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,10 @@
 # Copyright (c) 2022 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
+# TODO: Need to build this first as the bond interface package depends on the date type definitions.
+# These should be moved into Interface.Common (see https://github.com/DACH-NY/daml-finance/issues/288)
+DAML_PROJECT=../package/main/daml/Daml.Finance.Common daml build
+
 # Build Interfaces
 DAML_PROJECT=../package/main/daml/Daml.Finance.Interface.Common daml build
 DAML_PROJECT=../package/main/daml/Daml.Finance.Interface.Asset daml build
@@ -9,9 +13,9 @@ DAML_PROJECT=../package/main/daml/Daml.Finance.Interface.Settlement daml build
 DAML_PROJECT=../package/main/daml/Daml.Finance.Interface.Lifecycle daml build
 DAML_PROJECT=../package/main/daml/Daml.Finance.Interface.Derivative daml build
 DAML_PROJECT=../package/main/daml/Daml.Finance.Interface.Equity daml build
+DAML_PROJECT=../package/main/daml/Daml.Finance.Interface.Bond daml build
 
 # Build Implementations
-DAML_PROJECT=../package/main/daml/Daml.Finance.Common daml build
 DAML_PROJECT=../package/main/daml/Daml.Finance.Asset daml build
 DAML_PROJECT=../package/main/daml/Daml.Finance.Settlement daml build
 DAML_PROJECT=../package/main/daml/Daml.Finance.Lifecycle daml build
