f72210ad7141eee4e157818b51504ce4117a820d
Matteo Limberto <m.limberto@gmail.com>
2022-08-18 09:27:41 +0200

    import built da theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Daml Finance'
-copyright = '2022, Digital Asset - Financial Engineering'
-author = 'Digital Asset - Financial Engineering'
+project = u'Daml Finance'
+copyright = u'Copyright (c) 2022 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved. Any unauthorized use, duplication or distribution is strictly prohibited. "Digital Asset" and "Daml" are Registered in the U.S. Patent and Trademark Office.'
+author = u'Digital Asset'
 
 
 # -- General configuration ---------------------------------------------------
@@ -28,10 +28,18 @@ author = 'Digital Asset - Financial Engineering'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.extlinks',
+    'sphinx_copybutton',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
+
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+#
+# source_suffix = ['.rst', '.md']
+source_suffix = '.rst'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -44,7 +52,17 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'da_theme'
+html_theme_path = ['../theme']
+
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+html_theme_options = {
+    'collapse_navigation': False,
+    'index_page_boxes': True,
+    # 'pdf_download': True
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
