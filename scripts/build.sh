dad96dcbf7e8d93532ddd5b76674d24819c421aa
Johan Sjödin <jsjoedin@gmail.com>
2022-08-23 22:42:23 +0200

    removed unnecessary data-dependencies + moved test utility functions to Daml.Finance.Test.Util

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,14 +7,12 @@
 DAML_PROJECT=../package/main/daml/Daml.Finance.Common daml build
 
 ## Build Core
-
 # Interfaces
 DAML_PROJECT=../package/main/daml/Daml.Finance.Interface.Common daml build
 DAML_PROJECT=../package/main/daml/Daml.Finance.Interface.Asset daml build
 DAML_PROJECT=../package/main/daml/Daml.Finance.Interface.Instrument.Base daml build
 DAML_PROJECT=../package/main/daml/Daml.Finance.Interface.Settlement daml build
 DAML_PROJECT=../package/main/daml/Daml.Finance.Interface.Lifecycle daml build
-
 # Implementations
 DAML_PROJECT=../package/main/daml/Daml.Finance.Holding daml build
 DAML_PROJECT=../package/main/daml/Daml.Finance.Settlement daml build
@@ -23,18 +21,16 @@ DAML_PROJECT=../package/main/daml/Daml.Finance.Instrument.Base daml build
 DAML_PROJECT=../package/main/daml/Daml.Finance.RefData daml build
 
 ## Build Extensions
-
 # Interfaces
 DAML_PROJECT=../package/main/daml/Daml.Finance.Interface.Instrument.Generic daml build
 DAML_PROJECT=../package/main/daml/Daml.Finance.Interface.Instrument.Bond daml build
 DAML_PROJECT=../package/main/daml/Daml.Finance.Interface.Instrument.Equity daml build
-
 # Implementations
 DAML_PROJECT=../package/main/daml/Daml.Finance.Instrument.Generic daml build
 DAML_PROJECT=../package/main/daml/Daml.Finance.Instrument.Bond daml build
 DAML_PROJECT=../package/main/daml/Daml.Finance.Instrument.Equity daml build
 
-# Copy package dars into a dedicated folder
+## Copy package dars into a dedicated folder
 if [[ -d ../.dars ]]; then
   rm -r ../.dars
 fi
@@ -42,15 +38,13 @@ mkdir ../.dars
 cp ../package/main/daml/*/.daml/dist/* ../.dars/
 
 ## Build Tests
-
-# Core
+# Util
 DAML_PROJECT=../package/test/daml/Daml.Finance.Test.Util daml build
+# Core
 DAML_PROJECT=../package/test/daml/Daml.Finance.Common.Test daml build
 DAML_PROJECT=../package/test/daml/Daml.Finance.Holding.Test daml build
 DAML_PROJECT=../package/test/daml/Daml.Finance.Settlement.Test daml build
 DAML_PROJECT=../package/test/daml/Daml.Finance.RefData.Test daml build
-
 # Extensions
 DAML_PROJECT=../package/test/daml/Daml.Finance.Instrument.Generic.Test daml build
-DAML_PROJECT=../package/test/daml/Daml.Finance.Instrument.Equity.Test daml build
 DAML_PROJECT=../package/test/daml/Daml.Finance.Instrument.Bond.Test daml build
