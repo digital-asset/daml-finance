d21e35058d128baa152d38422d8695d3aa05a420
Brian Weir <brian.d.weir@gmail.com>
2023-02-14 11:31:15 +0100

    More testing

--- nix/packell.nix
+++ nix/packell.nix
@@ -7,15 +7,22 @@ let
   tarball = fetchTarball {
     url = "https://github.com/digital-asset/daml-finance/releases/download/packell/${version}/packell-${platform}.tar.gz";
   };
+  nativeBuildInputs = if stdenv.isLinux then [ pkgs.autoPatchelfHook ] else [ ];
+  buildInputs = if stdenv.isLinux then [ pkgs.glibc pkgs.gmp pkgs.libffi] else [ ];
+  # pkgs.lib.makeLibraryPath [ pkgs.libffi pkgs.gmp pkgs.glibc ]
 in
   stdenv.mkDerivation {
     name = "packell";
     version = "$version";
     src = tarball;
-    nativeBuildInputs = [ pkgs.autoPatchelfHook ];
+    nativeBuildInputs = nativeBuildInputs;
+    buildInputs = buildInputs;
     baseInputs = [ pkgs.binutils ];
     installPhase = ''
       mkdir -p $out/bin
       cp packell $out/bin
     '';
   }
+
+# To run manually, start with this
+# patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" packell
