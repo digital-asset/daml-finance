18b8cbf7a441edace6d33aabfcf0aba72a5f1f00
Brian Weir <brian.d.weir@gmail.com>
2023-02-14 14:08:27 +0100

    Update readme.md

--- nix/packell.nix
+++ nix/packell.nix
@@ -1,8 +1,12 @@
-# For the Linux version of Packell, the generated executable is 'Dynamically Linked' meaning the
-# interpreter and path of the executable is linked to libraries available on the build system at
-# build time. To fix this, we can use `autoPatchelfHook` from nix to automatically set the
-# interpreter and the 'rpath' of required libraries via 'buildInputs'.
-# Note - the Apple compiled Packell builds with everything necessary to run the executable.
+# For the Linux version of Packell, the compiled executable is 'Dynamically Linked' meaning the
+# interpreter and the paths (or 'rpaths') of the compiled executable is linked to
+# the interpreter/libraries available on the build system (ie., in its nix store). As the nix paths
+# will differ between the build and the host system, running Packell on a host system will generate
+# an error due to the differing paths of the interpreter/libraries. To fix this we use
+# `autoPatchelfHook` to automatically set both the interpreter and the 'rpath' of the dependending
+# libraries used by the executable (depending libraries are set in 'buildInputs').
+#
+# The Apple compiled Packell ships with everything necessary to run the executable.
 #
 # See - https://nixos.wiki/wiki/Packaging/Binaries
 #     - https://unix.stackexchange.com/questions/522822/different-methods-to-run-a-non-nixos-executable-on-nixos
