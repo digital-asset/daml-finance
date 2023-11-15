7fe57a0595731e48a14ab2ba519c3f14f80f7515
Gary Verhaegen <gary.verhaegen@digitalasset.com>
2023-11-15 13:59:29 +0100

    daml from artifactory

--- nix/daml.nix
+++ nix/daml.nix
@@ -1,16 +1,44 @@
-{ stdenv, jdk, sdkVersion, damlVersion }:
+{ stdenv, jdk, curl, curl_cert, sdkVersion, damlVersion, os, hashes }:
 let
-  os = if stdenv.isDarwin then "macos" else "linux";
-  tarball = fetchTarball {
+  os_tarball = fetchTarball {
     url = "https://github.com/digital-asset/daml/releases/download/v${damlVersion}/daml-sdk-${sdkVersion}-${os}.tar.gz";
+  };
+  tarball = stdenv.mkDerivation {
+    pname = "daml-tarball";
+    version = version;
+    src = ./..;
+    buildInputs = [ curl ];
+    SSL_CERT_FILE = curl_cert;
+    impureEnvVars = [ "ARTIFACTORY_USERNAME" "ARTIFACTORY_PASSWORD" ];
+    buildPhase = ''
+      set -euo pipefail
+
+      if [ -n "''${ARTIFACTORY_PASSWORD:-}" ]; then
+        curl -u $ARTIFACTORY_USERNAME:$ARTIFACTORY_PASSWORD \
+             https://digitalasset.jfrog.io/artifactory/assembly/daml/${version}/daml-sdk-${version}-${os}.tar.gz \
+          > $out
+      else
+        echo "ARTIFACTORY_USERNAME and ARTIFACTORY_PASSWORD must be set." >&2
+        exit 1
+      fi
+    '';
+    dontInstall = true;
+    outputHashAlgo = "sha256";
+    outputHashMode = "recursive";
+    outputHash = hashes.${os};
   };
 in
   stdenv.mkDerivation {
-    name = "daml-sdk";
-    version = "$sdkVersion";
+    pname = "daml-sdk";
+    version = sdkVersion;
     src = tarball;
-    buildPhase = "patchShebangs .";
-    installPhase = "DAML_HOME=$out ./install.sh";
+    dontUnpack = true;
+    buildPhase = ''
+      mkdir daml
+      tar xzf $src -C daml --strip-components 1
+      patchShebangs .
+    '';
+    installPhase = "cd daml; DAML_HOME=$out ./install.sh";
     propagatedBuildInputs = [ jdk ];
     preFixup = ''
       # Set DAML_HOME automatically.
