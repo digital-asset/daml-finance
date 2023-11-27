22bacf9d4081794afc184844775d632e3e1658dc
Johan Sjödin <johan.sjodin@digitalasset.com>
2023-11-27 18:16:49 +0100

    test from artifactory

--- nix/daml.nix
+++ nix/daml.nix
@@ -1,16 +1,47 @@
-{ stdenv, jdk, sdkVersion, damlVersion }:
+{ stdenv, jdk, curl, curl_cert, version, os, hashes }:
 let
-  os = if stdenv.isDarwin then "macos" else "linux";
-  tarball = fetchTarball {
-    url = "https://github.com/digital-asset/daml/releases/download/v${damlVersion}/daml-sdk-${sdkVersion}-${os}.tar.gz";
+  tarball = stdenv.mkDerivation {
+    pname = "daml-tarball";
+    version = version;
+    src = ./..;
+    buildInputs = [ curl ];
+    SSL_CERT_FILE = curl_cert;
+    impureEnvVars = [ "ARTIFACTORY_USERNAME" "ARTIFACTORY_PASSWORD" "ARTIFACTORY_TOKEN" ]; # Include all relevant variables
+    buildPhase = ''
+      set -euo pipefail
+
+      if [ -n "''${ARTIFACTORY_USERNAME:-}" ] && [ -n "''${ARTIFACTORY_PASSWORD:-}" ]; then
+        # Use username and password for authentication
+        curl -u $ARTIFACTORY_USERNAME:$ARTIFACTORY_PASSWORD \
+             https://digitalasset.jfrog.io/artifactory/assembly/daml/${version}/daml-sdk-${version}-${os}.tar.gz \
+          > $out
+      elif [ -n "''${ARTIFACTORY_TOKEN:-}" ]; then
+        # Use token for authentication
+        curl -H "Authorization: Bearer $ARTIFACTORY_TOKEN" \
+             https://digitalasset.jfrog.io/artifactory/assembly/daml/${version}/daml-sdk-${version}-${os}.tar.gz \
+          > $out
+      else
+        echo "Either ARTIFACTORY_USERNAME and ARTIFACTORY_PASSWORD or ARTIFACTORY_TOKEN must be set." >&2
+        exit 1
+      fi
+    '';
+    dontInstall = true;
+    outputHashAlgo = "sha256";
+    outputHashMode = "recursive";
+    outputHash = hashes.${os};
   };
 in
   stdenv.mkDerivation {
-    name = "daml-sdk";
-    version = "$sdkVersion";
+    pname = "daml-sdk";
+    version = version;
     src = tarball;
-    buildPhase = "patchShebangs .";
-    installPhase = "DAML_HOME=$out ./install.sh";
+    dontUnpack = true;
+    buildPhase = ''
+      mkdir daml
+      tar xzf $src -C daml --strip-components 1
+      patchShebangs .
+    '';
+    installPhase = "cd daml; DAML_HOME=$out ./install.sh";
     propagatedBuildInputs = [ jdk ];
     preFixup = ''
       # Set DAML_HOME automatically.
