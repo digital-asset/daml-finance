a08ee65c44f538de30cf2c559a6923558f790b65
Markus Friberg <markus.friberg@digitalasset.com>
2024-07-04 08:12:26 +0200

    update linux hash

--- shell.nix
+++ shell.nix
@@ -19,7 +19,7 @@ let
                        curl = pkgs.curl;
                        curl_cert = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
                        os = if pkgs.stdenv.isDarwin then "macos" else "linux";
-                       hashes = { linux = "1y+AixwC6+yLBk7M2igPsDy3gf+F1ZtvesFFyLQ+V84=";
+                       hashes = { linux = "0E8BOpw3ojfnTCzPKno0An3YMwRjr7yo/PwbOwNb7/A=";
                                   macos = "hZ7NQp1nu1JMn/B8kC7lsQE9p2yTd9QB3q2M5kKDgFQ="; };});
 in
 pkgs.mkShell {
