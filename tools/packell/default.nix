1a33c89316b1e540e14d2504679e0e27ffb6ff96
Karen Staner <karen.staner@digitalasset.com>
2025-12-02 11:03:02 +0100

    Fix DAR naming and local data-dependency resolution in packell

--- tools/packell/default.nix
+++ tools/packell/default.nix
@@ -2,7 +2,10 @@ let
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs {};
   hls = pkgs.haskell-language-server.override { supportedGhcVersions = [ "8107" ]; };
-  src = builtins.path { path = ./.; name = "packell"; };
+  src = pkgs.lib.cleanSourceWith {
+    src = ./.;
+    filter = path: type: true;
+  };
   packell = pkgs.haskell.packages.ghc8107.callCabal2nix "packell" src {};
 in
 pkgs.mkShell {
@@ -14,4 +17,7 @@ pkgs.mkShell {
     pkgs.ghc
     pkgs.haskellPackages.cabal-install
   ];
+  shellHook = ''
+    echo "✔ Using LOCAL packell source from: $(pwd)"
+  '';
 }
