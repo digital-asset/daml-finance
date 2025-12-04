46bee53263b9d43cb3314154c8e75c78717b761e
Karen Staner <karen.staner@digitalasset.com>
2025-12-04 13:36:31 +0100

    Default.nix that passed

--- tools/packell/default.nix
+++ tools/packell/default.nix
@@ -1,15 +1,12 @@
 let
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs {};
-
   hls = pkgs.haskell-language-server.override { supportedGhcVersions = [ "8107" ]; };
-
   src = pkgs.lib.cleanSourceWith {
     src = ./.;
     filter = path: type: true;
   };
-
-  packell = pkgs.haskellPackages.ghc8107.callCabal2nix "packell" src {};
+  packell = pkgs.haskell.packages.ghc8107.callCabal2nix "packell" src {};
 in
 pkgs.mkShell {
   buildInputs = [
@@ -20,7 +17,6 @@ pkgs.mkShell {
     pkgs.ghc
     pkgs.haskellPackages.cabal-install
   ];
-
   shellHook = ''
     echo "Using LOCAL packell source from: $(pwd)"
   '';
