ad1acb593762186b1f6efd1b316c1bd2b64aad46
Karen Staner <karen.staner@digitalasset.com>
2025-12-04 12:36:47 +0100

    Fix default.nix

--- tools/packell/default.nix
+++ tools/packell/default.nix
@@ -1,23 +1,9 @@
+{ pkgs ? import ./nix/sources.nix {} }:
+
 let
-  sources = import ./nix/sources.nix;
-  pkgs = import sources.nixpkgs {};
-  hls = pkgs.haskell-language-server.override { supportedGhcVersions = [ "8107" ]; };
   src = pkgs.lib.cleanSourceWith {
     src = ./.;
     filter = path: type: true;
   };
-  packell = pkgs.haskell.packages.ghc8107.callCabal2nix "packell" src {};
 in
-pkgs.mkShell {
-  buildInputs = [
-    hls
-    packell
-    pkgs.file
-    pkgs.binutils
-    pkgs.ghc
-    pkgs.haskellPackages.cabal-install
-  ];
-  shellHook = ''
-    echo "Using LOCAL packell source from: $(pwd)"
-  '';
-}
+pkgs.haskell.packages.ghc8107.callCabal2nix "packell" src {}
