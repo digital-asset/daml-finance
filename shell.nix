cb01f835f7fca733ed072c11229875cd2f221e94
Karen Staner <karen.staner@digitalasset.com>
2025-12-08 11:32:18 +0100

    Upgrade to SDK 3.4.9, update generated docs, and update package configs for multi-package migration

--- shell.nix
+++ shell.nix
@@ -39,7 +39,7 @@ let
     osJFrog = "${os}-${arch}";
     hashes = {
                 #base64 hashes from update-daml-hashes
-      linux = "Kt3UHV6Md/tc7e2g7f0icgf7CsFvhwi6xHSXFxBzan4=";
+      linux = "zPPJJfor22GHpovh2HOJH7AKQLfSW9p0UPgcZCdhSGM=";
       macos = "hITo4qlasMbhuLGfUwGMhuvkwVRaNgWQLdl6mEDx2Ew=";
     };
   };
